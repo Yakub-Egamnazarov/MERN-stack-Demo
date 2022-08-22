a48663b75b64d07eaf2ac0893c3db672a71502e0
Yakub <yakub.egamnazarov@mail.ru>
2022-08-22 17:25:12 +0800

    express app setup finished

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 1. create a backend folder with server file inside for Express node app.
 2. Express package installed
 3. Setting up the express app in server.js
+4. add dev script to package.json file, so it is easier to run command npm run dev
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require("express");
 // CREATE express app
 const app = express();
 
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the app" });
+});
+
 // LISTEN FOR REQUESTS
 app.listen(4000, () => {
   console.log("listening on port:4000");
